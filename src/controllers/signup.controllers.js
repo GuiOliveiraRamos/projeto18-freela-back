37523b7bfd114881c27d2a9eba6b1d0b444623a0
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-08-09 10:26:36 -0300

    signUp and signIn

--- src/controllers/signup.controllers.js
+++ src/controllers/signup.controllers.js
@@ -1,17 +1,51 @@
+import { hashSync } from "bcrypt";
 import { db } from "../database/database.connection.js";
 
 export async function signUp(req, res) {
-  const { name, phone, cpf, email, password, confirmPassword } = req.body;
+  const { name, phone, cpf, email, password } = req.body;
+
+  try {
+    const alreadyExists = await db.query(
+      `SELECT id FROM users WHERE email = $1`
+    );
+    const getExistsValues = [email];
+    const result = await db.query(alreadyExists, getExistsValues);
+
+    if (result.rowCount > 0)
+      return res.status(409).send("Usuário já cadastrado");
+
+    const hash = bcrypt.hash(password, 10);
+
+    const insertUserQuery = `INSERT INTO users (name, phone, cpf, email, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, now(), now())`;
+    const getUserValues = [name, phone, cpf, email, hash];
+    await db.query(insertUserQuery, getUserValues);
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-try {
-  const alreadyExists = await db.query(`SELECT id FROM users WHERE email = $1`);
-  const getValues = [email];
-  const result = await db.query(alreadyExists, getValues);
+export async function signIn(req, res) {
+  const { email, password } = req.body;
 
-  if (result.rowCount > 0) return res.status(409).send("Usuário já cadastrado");
+  try {
+    const userQuery = `SELECT id FROM users WHERE email = $1`;
+    const getUserValues = [email];
+    const userResult = await db.query(userQuery, getUserValues);
 
-  const hash = bcrypt.hash(password, 10);
-} catch (error) {
-  console.log(error);
+    if (userResult.rowCount === 0) return res.sendStatus(401);
+
+    const user = userResult.rows[0];
+
+    const validatePassword = hashSync(password, user.password);
+
+    if (!validatePassword) return res.sendStatus(401);
+
+    await db.query(`UPDATE users SET updated_at = now() WHERE id = $1`, [
+      user.id,
+    ]);
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+  }
 }
