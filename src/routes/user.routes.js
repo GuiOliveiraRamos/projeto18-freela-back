e115f94a80f22ed3d95895bfd4315d441a0a4c83
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-08-09 10:49:50 -0300

    feat: routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { schemaValidation } from "../middlewares/schema.validation.js";
 import { schemaSignIn, schemaSignUp } from "../schemas/users.schemas.js";
-import { signUp } from "../controllers/signup.controllers.js";
+import { signIn, signUp } from "../controllers/signup.controllers.js";
 
 const signRoutes = Router();
 
 signRoutes.post("/signup", schemaValidation(schemaSignUp), signUp);
-signRoutes.post("/signin", schemaValidation(schemaSignIn));
+signRoutes.post("/signin", schemaValidation(schemaSignIn), signIn);
 
 export default signRoutes;
