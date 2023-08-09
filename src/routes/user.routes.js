6e7a8285df994f371060dac08ba73ff17b9d20d2
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-08-09 10:26:36 -0300

    feat:signUp and signIn
    :wq
    
    :wq

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { schemaValidation } from "../middlewares/schema.validation.js";
 import { schemaSignIn, schemaSignUp } from "../schemas/users.schemas.js";
+import { signUp } from "../controllers/signup.controllers.js";
 
 const signRoutes = Router();
 
-signRoutes.post("/signup", schemaValidation(schemaSignUp));
+signRoutes.post("/signup", schemaValidation(schemaSignUp), signUp);
 signRoutes.post("/signin", schemaValidation(schemaSignIn));
 
 export default signRoutes;
