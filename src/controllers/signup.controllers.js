de8bfd1b007f62fac9d3fd1906573bde01051566
Guilherme Oliveira <121904094+GuiOliveiraRamos@users.noreply.github.com>
2023-12-10 14:05:02 -0300

    feat: Update signup.controllers.js

--- src/controllers/signup.controllers.js
+++ src/controllers/signup.controllers.js
@@ -4,18 +4,13 @@ import { v4 as uuidv4 } from "uuid";
 
 export async function signUp(req, res) {
   const { name, phone, cpf, email, password, confirmPassword } = req.body;
-}
-
 try {
   const alreadyExists = await db.query(`SELECT id FROM users WHERE email = $1`);
   const getValues = [email];
   const result = await db.query(alreadyExists, getValues);
-
     if (result.rowCount > 0)
       return res.status(409).send("Usuário já cadastrado");
-
     const hash = bcrypt.hashSync(password, 10);
-
     const insertUserQuery = `INSERT INTO users (name, phone, cpf, email, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, now(), now())`;
     const getUserValues = [name, phone, cpf, email, hash];
     await db.query(insertUserQuery, getUserValues);
@@ -27,31 +22,20 @@ try {
 
 export async function signIn(req, res) {
   const { email, password } = req.body;
-
-  try {
     const userQuery = `SELECT id, password FROM users WHERE email = $1`;
     const getUserValues = [email];
     const userResult = await db.query(userQuery, getUserValues);
-
     if (userResult.rowCount === 0) return res.sendStatus(401);
-
     const user = userResult.rows[0];
     const hashedPassword = user.password;
     const validatePassword = bcrypt.compareSync(password, hashedPassword);
-
     if (!validatePassword) return res.sendStatus(401);
-
     const sessionId = uuidv4();
     const sessionQuery = `INSERT INTO sessions (user_id,session_id,created_at) VALUES ($1, $2, now())`;
     const sessionValues = [user.id, sessionId];
-
     await db.query(sessionQuery, sessionValues);
-
     await db.query(`UPDATE users SET updated_at = now() WHERE id = $1`, [
       user.id,
     ]);
     res.status(200).json({ sessionId });
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
 }
